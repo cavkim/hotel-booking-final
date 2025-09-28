7c1935d2a1060b5f39b42aab947b5a7a0b428abf
Eath Mengsrong <athmengsrong2@gmail.com>
2025-09-28 23:00:54 +0700

    Reorganize: Create clean frontend folder structure

--- front-end/src/constants/components/header.js
+++
@@ -1,9 +0,0 @@
-const header = [
-  {
-    title: "ទំព័រដើម",
-    path: "ទំព័រដើម",
-  },
-];
-
-
-// In constants -> components = Wook all logic file here 
--- front-end/src/modules/detail/core/request.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export const reqGetRelatedPosts = () => {
-  return axios.get(``);
-};
